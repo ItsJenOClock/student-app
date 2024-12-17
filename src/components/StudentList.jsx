bd9e00c2f93ac4f0ace4134b532af1778b42013e
Jen Nguyen <26128269+ItsJenOClock@users.noreply.github.com>
2024-12-16 21:14:08 -0600

    iterate through with array of JSX elements

--- src/components/StudentList.jsx
+++ src/components/StudentList.jsx
@@ -2,18 +2,35 @@ import './StudentList.css';
 import Student from './Student';
 
 const StudentList = () => {
-const headingClass = 'student-list__heading';
-const listClass = 'student-list';
-return (
-    <section>
-        <h2 className={headingClass}>Student List</h2>
-        <ul className={listClass}>
-          <li><Student name="Ada" email="ada@dev.org"></Student></li>
-          <li><Student name="Soo-ah" email="sooah@dev.org"></Student></li>
-          <li><Student name="Chrissy" email="chrissy@dev.org"></Student></li>
-        </ul>
-    </section>
-);
+  const studentData = [
+      {
+          nameData: 'Ada',
+          emailData: 'ada@dev.org'
+      },
+      {
+          nameData: 'Soo-ah',
+          emailData: 'sooah@dev.org'
+      },
+      {
+          nameData: 'Chrissy',
+          emailData: 'chrissy@dev.org'
+      }
+  ];
+
+  const studentComponents = studentData.map(student => {
+      return (
+          <li><Student name={student.nameData} email={student.emailData}></Student></li>
+      );
+  });
+
+  return (
+      <section>
+          <h2>Student List</h2>
+          <ul>
+              {studentComponents}
+          </ul>
+      </section>
+  );
 };
 
 export default StudentList;
